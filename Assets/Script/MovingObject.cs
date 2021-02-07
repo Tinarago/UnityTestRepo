eaf3057cc37cefb6ff6d89dc9ec557d9aec157eb
Tina Kim <tinadays123@gmail.com>
2021-02-07 17:52:37 +0900

    Script modified

--- Assets/Script/MovingObject.cs
+++ Assets/Script/MovingObject.cs
@@ -7,12 +7,12 @@ public class MovingObject : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // 민주야 사랑해 하트 뿅뿅 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 나도 사랑해 뿅뿅 훈션 섹스
     }
 }
